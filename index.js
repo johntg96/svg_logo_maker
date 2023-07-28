5f3417b81be36cb017bdd5afc2c300bd204970f4
John Garrison <splendidonkey@gmail.com>
2023-07-28 16:12:35 -0500

    renamed "shapes.js" to "render.js" and added shape as an attribute and another argument of creating the overall .svg

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // index.js
 const fs = require('fs');
 const  { askQuestions } = require('./lib/questions');
-const { createTriangle, createSquare, createCircle } = require('./lib/shapes');
+const { renderSVG } = require('./lib/render');
 
 async function main() {
   try {
     const shapeDescription = await askQuestions();
     console.log(shapeDescription);
     // Now the shape input is given back as an object, it can be used/accessed from here..
-    
+    // create colored shape here:
+    renderSVG(shapeDescription.logoText, shapeDescription.logoTextColor, shapeDescription.logoShape, shapeDescription.logoBgColor);
+
+
   } catch (err) {
     console.error('Error:', err.message);
   }

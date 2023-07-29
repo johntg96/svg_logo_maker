6d6280a8e373ab542434cd92f8124468379bd9e0
John Garrison <splendidonkey@gmail.com>
2023-07-29 08:57:05 -0500

    restructured render, moved write function to from render file to index file. Added syntax for generating SVG syntax in template literal

--- index.js
+++ index.js
@@ -2,15 +2,33 @@
 const fs = require('fs');
 const  { askQuestions } = require('./lib/questions');
 const { renderSVG } = require('./lib/render');
+const outputCyanText = (text) => console.log(`\x1b[36m${text}\x1b[0m`);
 
 async function main() {
   try {
+    // obtain svg criteria from user
     const shapeDescription = await askQuestions();
     console.log(shapeDescription);
-    // Now the shape input is given back as an object, it can be used/accessed from here..
-    // create colored shape here:
-    renderSVG(shapeDescription.logoText, shapeDescription.logoTextColor, shapeDescription.logoShape, shapeDescription.logoBgColor);
+    
+    // set default file name
+    if (shapeDescription.fileName === '') {
+      shapeDescription.fileName = "logo";
+    }
 
+    // take the users svg criteria and render the SVG string
+    svgString = renderSVG(shapeDescription.logoText, shapeDescription.logoTextColor, shapeDescription.logoShape[0], shapeDescription.logoBgColor);
+
+    // append file system directory to file name path
+    svgFile = `./generated_SVG/${shapeDescription.fileName}.svg`;
+
+    // create the svg file and write the string to it
+    fs.writeFile(svgFile, svgString, (error) => {
+      if (error) {
+        throw error;
+      } else {
+        outputCyanText(`\nSVG created! ${shapeDescription.fileName}\n`);
+      }
+    });
 
   } catch (err) {
     console.error('Error:', err.message);

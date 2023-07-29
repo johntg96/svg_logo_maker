67e267d475349ddd3bc69c6ce5079d664987e548
John Garrison <splendidonkey@gmail.com>
2023-07-29 11:14:28 -0500

    very minor. added comment and added gitignore entry

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ async function main() {
 
     let shape;
     
+    // instantiate new obj depending on choice of shape
     if (shapeDescription.logoShape[0] == 'circle') {
       shape = new Circle(shapeDescription.logoBgColor);
     } else if (shapeDescription.logoShape[0] == 'square') {
